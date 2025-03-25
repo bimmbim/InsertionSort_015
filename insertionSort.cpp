5348ee2a1a5af0104f8f82d569cf712dca5f593b
bimmbim <bimabaraja76@gmail.com>
2025-03-25 11:36:51 +0700

    commit 4

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -24,4 +24,16 @@ cout << endl;
 cout << "======================" << endl;
 cout << "Masukkan Element Array" << endl;
 cout << "======================" << endl;
+
+for (int i = 0; i < n; i++)
+{
+cout << "Data ke-" << (i+1) << ": ";
+cin >> arr[i];
+}
+}
+
+void insertionSort()
+{
+int temp;
+int j, i;
 }
